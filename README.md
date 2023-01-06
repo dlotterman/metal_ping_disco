f8c1ce562f6aa39faec2243cba9f784b1b1a1759
dlotterman <dlotterman@users.noreply.github.com>
2023-01-06 17:27:02 +0000

    updates to include mysql

--- README.md
+++ README.md
@@ -41,3 +41,10 @@ export METAL_PING_DISCO_AWS_S3_ENDPOINT=
 ```
 
 These set the s3 endpoint (and provider), bucket name and credentials both applications will use. Note that the s3 provider does not need to be AWS s3 itself, just s3 protocol compliant. 
+
+Optionally, set:
+`export METAL_PING_DISCO_MYSQL_ENDPOINTS_RAW="haha_db:127.0.0.1:3306:disco_user:disco_Pass:disco_db,haha_db2:127.0.0.2:3306:disco_user:disco_Pass:disco_db"`
+
+This will enable tell the container to make a MySQL query and measure the latency for:
+	* `127.0.0.1` on port `3306` with the user `disco_user` with the password `disco_Pass`, where the line on the graph and legend will be `haha_db`, and then all the same for `haha_db2` on IP `127.0.0.2`.
+	* Values for each mysql instance are colon seperated, mysql instances themselves are comma seperated.
--- metal_ping_disco_data_runner.py
+++ metal_ping_disco_data_runner.py
@@ -4,9 +4,11 @@ import subprocess
 import logging
 import os
 import sys
+import time
 
 import pandas as pd
 
+import mysql.connector as mysqldb
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s %(message)s", datefmt="%Y-%m-%dT%H:%M:%S%z"
@@ -16,6 +18,10 @@ AWS_S3_BUCKET = os.getenv("METAL_PING_DISCO_AWS_S3_BUCKET")
 AWS_ACCESS_KEY_ID = os.getenv("METAL_PING_DISCO_AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("METAL_PING_DISCO_AWS_SECRET_ACCESS_KEY")
 AWS_S3_ENDPOINT = os.getenv("METAL_PING_DISCO_AWS_S3_ENDPOINT")
+MYSQL_ENDPOINTS_RAW = os.getenv("METAL_PING_DISCO_MYSQL_ENDPOINTS_RAW")
+
+MYSQL_ENDPOINTS = [
+]
 
 endpoints = [
     "metal_dc13:136.144.56.179",
@@ -29,10 +35,14 @@ endpoints = [
     "opendns:208.67.222.222",
 ]
 
+def process_mysql_endpoints():
+
+    for ENDPOINT in MYSQL_ENDPOINTS_RAW.split(','):
+        MYSQL_ENDPOINTS.append(ENDPOINT)
 
 def metal_ping_disco():
-
     logging.info("starting metal_ping_disco data collection")
+    process_mysql_endpoints()
 
     try:
         network_data = pd.read_csv(
@@ -69,6 +79,35 @@ def metal_ping_disco():
         network_data = pd.concat(
             [network_data, network_data_collected], ignore_index=True
         )
+    for mysql_endpoint in MYSQL_ENDPOINTS:
+        name = mysql_endpoint.split(':')[0]
+        host = mysql_endpoint.split(':')[1]
+        port = mysql_endpoint.split(':')[2]
+        username = mysql_endpoint.split(':')[3]
+        password = mysql_endpoint.split(':')[4]
+        database = mysql_endpoint.split(':')[5]
+        tic = time.time()
+        try:
+            connection = mysqldb.connect(
+                user=username,
+                password=password,
+                host=host,
+                database=database)
+            if connection.is_connected():
+                dbinfo = connection.get_server_info()
+                toc = time.perf_counter()
+                connection.close()
+                latency = time.time() - tic
+        except Error as e:
+            logging.error('Error connecting to %s mysql endpoint', mysql_endpoint)
+        finally:
+            logging.info('%s latency time is %.4f', mysql_endpoint, latency)
+            network_data_collected = pd.DataFrame(
+                [[name, ts, endpoint_latency]], columns=["ip", "time", "latency"]
+            )
+            network_data = pd.concat(
+                [network_data, network_data_collected], ignore_index=True
+            )          
 
     network_data.to_csv(
         "s3://{}/metal_ping_disco_network_data.csv".format(AWS_S3_BUCKET),
